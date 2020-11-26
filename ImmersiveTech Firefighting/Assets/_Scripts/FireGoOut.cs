93d91f9130bc1738dc0dda2b55e8e50a14ba977d
Unknown <robert.andersen@uoit.net>
2020-11-26 17:58:42 -0500

    Fire particles emit less when hit by certain amount of water and are destroyed when hit by 300 units of water.

---
+++ ImmersiveTech Firefighting/Assets/_Scripts/FireGoOut.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireGoOut : MonoBehaviour
+{
+    public int waterDroplets = 300;
+    public ParticleSystem fireParticles;
+    public ParticleSystem smokeParticles;
+
+    public float emissionValue = 4.0f;
+    public float smokEmissionValue = 10.0f;
+    //public int rateOverTime;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //void OnCollisionEnter(Collision other)
+    //{
+        //Debug.Log("Not Yet");
+        //if(other.gameObject.tag == "Water")
+        //{
+            //waterDroplets -= 1;
+            //Debug.Log(waterDroplets);
+            //checkFireOut();
+        //}
+    //}
+    void OnParticleCollision(GameObject other) 
+    {
+        if(other.tag == "Water")
+        {
+            checkFireOut();
+            waterDroplets -= 1;
+            //checkFireOut();
+            Debug.Log(waterDroplets);
+            
+        }
+    }
+
+    public void checkFireOut()
+    {   
+        
+        if(waterDroplets <= 0)
+        {
+            Debug.Log("Gets here");
+            Destroy(this.gameObject);
+        }
+        else if (waterDroplets <= 150)
+        {
+            var fireEmission = fireParticles.emission;
+            fireEmission.rateOverTime = emissionValue;
+
+            var smokeEmission = smokeParticles.emission;
+            smokeEmission.rateOverTime = smokEmissionValue;
+        }
+    }
+}
