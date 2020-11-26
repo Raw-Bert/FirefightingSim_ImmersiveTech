fac9c9d6962ea58ff2c598727c638ce8cdea0e8b
Unknown <robert.andersen@uoit.net>
2020-11-26 16:41:21 -0500

    Trees + cars have colliders. Added water particle effect, will do something with it when someone makes me models.

--- ImmersiveTech Firefighting/Assets/_Scripts/GameManager.cs
+++ ImmersiveTech Firefighting/Assets/_Scripts/GameManager.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+
+//Menu, with buttons
+//Timer
+//Water particle put out fire, also shoots when player pcisk up hose nozzle
+//Game win when put out all fire, time shown?
+
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float timer = 0.0f;
+    public int seconds;
 
     // Update is called once per frame
     void Update()
     {
-        
+        CountSeconds();
+    }
+    
+    public void CountSeconds()
+    {
+        timer += Time.deltaTime;
+        seconds = (int)(timer % 60);
+        Debug.Log(seconds);
     }
 }
