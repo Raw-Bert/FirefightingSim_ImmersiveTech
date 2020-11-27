d26c102b0d30f6a4c5a8a24f15659302a7c331ee
Unknown <robert.andersen@uoit.net>
2020-11-27 16:56:04 -0500

    Fixed minor issue where a non fatal error would appear.

--- ImmersiveTech Firefighting/Assets/_Scripts/ButtonTrigger.cs
+++ ImmersiveTech Firefighting/Assets/_Scripts/ButtonTrigger.cs
@@ -8,12 +8,14 @@ public class ButtonTrigger : MonoBehaviour
     public string sceneName;
     public GameObject player;
     public AudioSource click;
+    public GameObject tele;
     
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "ButtonTop")
         {
             click.Play();
+            Destroy(tele.gameObject);
             Destroy(player.gameObject);
             SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
         }
