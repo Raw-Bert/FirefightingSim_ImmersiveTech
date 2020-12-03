bda278bb04f81960b72c842a833350b0dd148ea6
Unknown <robert.andersen@uoit.net>
2020-12-02 20:25:35 -0500

    PArticles and testing

---
+++ ImmersiveTech Firefighting/Assets/_Scripts/YellowButtonTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.SceneManagement;
+
+
+public class YellowButtonTrigger : MonoBehaviour
+{
+    //public string sceneName;
+    //public GameObject player;
+    public AudioSource click;
+    //public GameObject tele;
+    
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("WTFLESS");
+        if (other.tag == "ButtonTop")
+        {
+            //Debug.Log("WTF");
+            click.Play();
+            //Destroy(tele.gameObject);
+            //Destroy(player.gameObject);
+            //SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+        }
+    }
+}
